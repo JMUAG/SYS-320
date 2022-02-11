ed09a2f68ede8fbc76ecf679c293a746d5e707d5
JMUAG <jahseemmaxwell@uagateway.org>
2022-02-11 02:41:38 +0000

    classwok4

---
+++ Week3/class/urlHaus2.py
@@ -0,0 +1,34 @@
+#
+import csv, sys, re
+#
+
+def urlHausOpen(filename,searchTerm):
+#
+
+    with open(filename, 'rt') as f:
+        contents = csv.reader(f)
+
+        #results = []
+
+    for _ in range(9):
+        next(contents)
+
+        print(_)
+    #for keyword in searchTerm:
+
+        #for eachLine in contents:
+           # x = re.findall(r''+keyword+'',eachLine[2])
+
+            #for z in x:
+
+                #results.append(z)
+
+# Don't edit this line. It is here to show how it is possible
+# to remove the "tt" so programs don't convert the malicious
+# domains to links that an be accidentally clicked on.
+    #the_url = eachLine[2].replace("http","hxxp")
+    #the_src = eachLine[4]
+    #print("""
+    #URL:
+    #Info: 
+    #{}""",format(the_url, the_src,"*"+60))
