fa1ed400e91705a90424281efba8c01ec434ae42
unknown <benhacked@emul.info>
2022-04-05 11:33:06 -0400

    week 10 hw

---
+++ Week10/homework/test.ps1
@@ -0,0 +1,3 @@
+$content = "If you want your files restored, please contact me at email@champlain.edu. I look forward to doing business with you."
+
+$content | Out-File -FilePath 'C:\Users\benhacked.EMUL\Desktop\Readme.READ'
